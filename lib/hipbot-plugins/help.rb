d80a7e476cd7083d64f541768ac9f7a39002e454
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2013-08-30 23:09:21 +0200

    hipchat emoticons ;)

--- lib/hipbot-plugins/help.rb
+++ lib/hipbot-plugins/help.rb
@@ -3,6 +3,19 @@ module Hipbot
     class Help
       include Hipbot::Plugin
 
+      desc 'lists all standard emoticons'
+      on /^emoticons/ do
+        reply ":-) :o :$ :( :-* :# :'( ;-) (embarrassed) :Z :p 8) O:) :-D (oops) :| (thumbsup) (thumbsdown) (allthethings) (android) (areyoukiddingme)" +
+              " (arrington) (ashton) (awyeah) (badpokerface) (basket) (beer) (bumble) (bunny) (cadbury) (cake) (candycorn) (caruso) (cereal)" +
+              " (challengeaccepted) (chewie) (chocobunny) (chompy) (chris) (coffee) (content) (cornelius) (dealwithit) (derp)" +
+              " (disapproval) (dosequis) (ducreux) (dumb) (facepalm) (fap) (foreveralone) (freddie) (fry) (fu) (fuckyeah) (garret) (gates) (ghost) (goodnews)"
+        # splitted for better formatting under Adobe AIR clients
+        reply "(greenbeer) (gtfo) (haveaseat) (heart) (hipchat) (hipster) (huh) (ilied) (itsatrap) (jackie) (jobs) (kennypowers) (krang) (kwanzaa) (lincoln)" +
+              " (lol) (lolwut) (megusta) (menorah) (notbad) (nothingtodohere) (ohcrap) (okay) (omg) (orly) (pbr) (pete) (philosoraptor) (pirate) (pokerface)" +
+              " (poo) (present) (pumpkin) (rageguy) (rebeccablack) (reddit) (rudolph) (sadpanda) (sadtroll) (samuel) (santa) (scumbag) (seomoz) (shamrock)" +
+              " (skyrim) (stare) (sweetjesus) (taft) (tree) (troll) (truestory) (turkey) (washington) (wat) (wtf) (yey) (yodawg) (yuno) (zoidberg) (zzz)"
+      end
+
       desc 'generates help for all reactions'
       on /^help$/ do
         reactions_with_description = Hipbot.plugin_reactions.reject{ |reaction| reaction.desc.nil? }
