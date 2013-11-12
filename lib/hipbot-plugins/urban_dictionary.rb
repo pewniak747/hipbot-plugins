96b6416818ac5545ca65033edebe675673077827
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2013-11-12 19:37:43 +0100

    use different wrapper

--- lib/hipbot-plugins/urban_dictionary.rb
+++ lib/hipbot-plugins/urban_dictionary.rb
@@ -1,4 +1,4 @@
-require 'urban'
+require 'urban_dictionary'
 
 module Hipbot
   module Plugins
@@ -7,11 +7,11 @@ module Hipbot
 
       desc 'explains a phrase using UrbanDictionary'
       on /^explain (.+)/ do |phrase|
-        entry = Urban::Dictionary.search(phrase)
-        if entry.definitions.nil? || entry.definitions.empty?
+        entry = ::UrbanDictionary.define(phrase)
+        if entry.entries.nil? || entry.entries.empty?
           reply("Congrats #{sender.first_name}, you just invented a completely new #{phrase.count(' ').zero? ? 'word' : 'phrase'}.")
         else
-          reply(entry.definitions.first)
+          reply(entry.entries.first.definition)
         end
       end
     end
