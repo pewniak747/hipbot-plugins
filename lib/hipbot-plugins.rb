3e4c8950760489637a0a6b8b78721e53275d7228
Ethan McGee <bulletshot60@gmail.com>
2014-05-30 17:24:34 -0400

    fix typo

--- lib/hipbot-plugins.rb
+++ lib/hipbot-plugins.rb
@@ -6,16 +6,13 @@ require "hipbot-plugins/help"
 require "hipbot-plugins/human"
 require "hipbot-plugins/github"
 require "hipbot-plugins/google"
-require "hipbot-plugins/numbers"
+require "hipbot-plugins/google"
 require "hipbot-plugins/wolfram_alpha"
 require "hipbot-plugins/meme_generator"
 require "hipbot-plugins/rapportive"
 
 module Hipbot
   module Plugins
-      desc 'list available memes to generate'
-      on /^memes/ do
-        reply(Generator.memes.keys.sort.join(', '))
-      end
+    # Your code goes here...
   end
 end
