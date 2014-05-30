83f78a9fe40c1ed404039f4978981ac99a6a0730
Ethan McGee <bulletshot60@gmail.com>
2014-05-30 17:17:04 -0400

    add new number trivia plugin

--- lib/hipbot-plugins.rb
+++ lib/hipbot-plugins.rb
@@ -6,12 +6,16 @@ require "hipbot-plugins/help"
 require "hipbot-plugins/human"
 require "hipbot-plugins/github"
 require "hipbot-plugins/google"
+require "hipbot-plugins/numbers"
 require "hipbot-plugins/wolfram_alpha"
 require "hipbot-plugins/meme_generator"
 require "hipbot-plugins/rapportive"
 
 module Hipbot
   module Plugins
-    # Your code goes here...
+      desc 'list available memes to generate'
+      on /^memes/ do
+        reply(Generator.memes.keys.sort.join(', '))
+      end
   end
 end
