62d1c24ad4b99714ad2972105dad8180233e0b94
bartoszkopinski <bartosz.kopinski@netguru.pl>
2014-06-05 19:44:27 +0200

    spec fix for ruby 2+

--- spec/meme_generator_spec.rb
+++ spec/meme_generator_spec.rb
@@ -1,25 +1,25 @@
 require 'spec_helper'
 
 describe Hipbot::Plugins::MemeGenerator::Generator do
-  let(:meme) { stub.as_null_object }
+  let(:meme) { double }
 
   describe "splitting text" do
     it "should split text according to quotation marks" do
       generator = described_class.new(meme, '"I don\'t always do a deploy" "but when I do, I do it on friday"')
-      generator.upper_text.should == "I don\'t always do a deploy"
-      generator.lower_text.should == "but when I do, I do it on friday"
+      expect(generator.upper_text).to eq "I don\'t always do a deploy"
+      expect(generator.lower_text).to eq "but when I do, I do it on friday"
     end
 
     it "should split text evenly" do
       generator = described_class.new(meme, 'I don\'t always do a deploy but when I do, I do it on friday')
-      generator.upper_text.should == "I don't always do a deploy but"
-      generator.lower_text.should == "when I do, I do it on friday"
+      expect(generator.upper_text).to eq "I don't always do a deploy but"
+      expect(generator.lower_text).to eq "when I do, I do it on friday"
     end
 
     it "should not split monolithic text" do
       generator = described_class.new(meme, 'ermagherdtestdrivendevelopment')
-      generator.upper_text.should == "ermagherdtestdrivendevelopment"
-      generator.lower_text.should == ""
+      expect(generator.upper_text).to eq "ermagherdtestdrivendevelopment"
+      expect(generator.lower_text).to eq ""
     end
   end
 end
